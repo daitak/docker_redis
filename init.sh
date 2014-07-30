8a8e2379563c1e0555030096dc7bdbaf8e50ffe1
Daiki Takasao <daiki.takasao@gmail.com>
2014-07-30 13:07:40 +0900

    modified for selecting sentinel quorum

--- init.sh
+++ init.sh
@@ -13,11 +13,22 @@ fi
 
 #Sentinel Config
 #
+if [ -z "$SENTINEL_QUORUM" ]; then
+  QUORUM=1
+else
+  numeric_check=`echo $SENTINEL_QUORUM | awk '/[^0-9]/ {print}'`
+  if [[ ! -z $numeric_check ]]; then
+    echo "SENTINEL_QUORUM is must be specified numeric"
+    exit 1
+  fi
+  QUORUM=$SENTINEL_QUORUM
+fi
+
 if [ -z "$REDIS_MASTER_PORT_6379_TCP_ADDR" ]; then
   echo "REDIS_MASTER_PORT_6379_TCP_ADDR not defined. Did you run with -link?";
-  sed -i -e "s/^sentinel monitor.*/sentinel monitor mymaster `ip -f inet addr show eth0 | awk '/inet/{ print $2}' | cut -d'/' -f1` 6379 1/g" /etc/redis-sentinel.conf
+  sed -i -e "s/^sentinel monitor.*/sentinel monitor mymaster `ip -f inet addr show eth0 | awk '/inet/{ print $2}' | cut -d'/' -f1` 6379 $QUORUM/g" /etc/redis-sentinel.conf
 else
-  sed -i -e "s/^sentinel monitor.*/sentinel monitor mymaster $REDIS_MASTER_PORT_6379_TCP_ADDR 6379 1/g" /etc/redis-sentinel.conf
+  sed -i -e "s/^sentinel monitor.*/sentinel monitor mymaster $REDIS_MASTER_PORT_6379_TCP_ADDR 6379 $QUORUM/g" /etc/redis-sentinel.conf
 fi
 service redis-sentinel start
 
